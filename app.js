40e36e249388fd2eb8c59e269f2cfac38ef8de32
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2022-02-02 18:16:53 +0600

    74

--- app.js
+++ app.js
@@ -20,10 +20,25 @@ const app = Vue.createApp({
 
 
 app.component('friend-contact',{
-    template:,
+    template:`
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails()">{{ detailsAreVisible?'Hide':'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+          <li><strong>Phone:</strong> {{ friend.phone }}</li>
+          <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+      </li>
+    `,
     data(){
         return{
-            detailsAreVisible:false
+            detailsAreVisible:false,
+            friend:{
+                id: 'manuel',
+                name: 'Manuel',
+                phone: '01478244',
+                email: 'manuel@gmail.com'
+            },
         }
     },
     methods:{
